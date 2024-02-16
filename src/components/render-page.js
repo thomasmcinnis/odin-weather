c46cc18e7620d0a37d54f2e33a05670f05f249a1
Thomas McInnis <hey@thomasmcinnis.com>
2024-02-16 17:09:33 +1100

    Render current weather

--- src/components/render-page.js
+++ src/components/render-page.js
@@ -1,9 +1,16 @@
 export function renderCurrent(location, currentData, prefersC) {
-    console.log(location, currentData, prefersC);
-
     const temp = prefersC ? currentData.temp_c : currentData.temp_f;
+    const roundedTemp = Math.round(temp);
+    const imgSrc = `https:${currentData.condition.icon}`;
+
+    const img = document.querySelector('.current-icon');
+    img.src = imgSrc;
+
+    const cityName = document.querySelector('#city-name');
+    cityName.textContent = location;
+
+    const currentTemp = document.querySelector('#current-temp');
+    currentTemp.textContent = `+ ${roundedTemp}º`;
 }
 
-export function renderForecast(forecastData, prefersC) {
-    console.log(forecastData, prefersC);
-}
+export function renderForecast(forecastData, prefersC) {}
